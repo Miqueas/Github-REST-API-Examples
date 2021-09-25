0ff7cda04e8372ae1e412cb0ac07bd23f02f16f3
Miqueas Martinez <miqueas2020@yahoo.com>
2021-09-24 23:47:00 -0500

    Cambios menores en el código Python

--- Github.py
+++ Github.py
@@ -1,118 +1,107 @@
-import http.client as Client
-import json        as JSON
-import argparse    as ArgParse
-import re          as RegExp
+import json
+import argparse
+import re
+from http import client
 
 USER_AGENT = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/89.0.4389.82 Safari/537.36"
 API_HOST = "api.github.com"
 API_PATH = "/users/"
 
-class GithubRepo(object):
-  def __init__(self, name, desc):
-    super(GithubRepo, self).__init__()
-    self.Name = name
-    self.Desc = desc
-
-
-class GithubUser(object):
-  def __init__(self, username):
-    super(GithubUser, self).__init__()
+class GithubUser:
+  def __init__(self, username: str):
     self.url = API_PATH + username
 
-    conn = Client.HTTPSConnection(API_HOST)
-    conn.request("GET", self.url, headers={ "User-Agent": USER_AGENT })
+    conn = client.HTTPSConnection(API_HOST)
+    conn.request("GET", self.url, headers = { "User-Agent": USER_AGENT })
 
     res = conn.getresponse()
-    self.json = JSON.loads(res.read())
+    self.json = json.loads(res.read())
 
-    self.Name      = self.json["name"]
-    self.Bio       = self.json["bio"]
-    self.Link      = self.json["html_url"]
-    self.Repos     = { "count": self.json["public_repos"], "arr": [] }
-    self.Gists     = { "count": self.json["public_gists"], "arr": [] }
-    self.Followers = { "count": self.json["followers"], "arr": [] }
-    self.Following = { "count": self.json["following"], "arr": [] }
+    self.name      = self.json["name"]
+    self.bio       = self.json["bio"]
+    self.link      = self.json["html_url"]
+
+    self.repos     = { "count": self.json["public_repos"], "arr": [] }
+    self.gists     = { "count": self.json["public_gists"], "arr": [] }
+    self.followers = { "count": self.json["followers"],    "arr": [] }
+    self.following = { "count": self.json["following"],    "arr": [] }
 
     res.close()
     conn.close()
 
-  def Fetch(self, kind):
-    assert (RegExp.match(r'(repos|gists|followers|following)', kind) is not None), f"Unsupported endpoint: {kind}"
+  def fetch(self, thing: str):
+    assert (re.match(r'(repos|gists|followers|following)', thing) is not None), f"Unsupported endpoint: {thing}"
 
-    url  = f"{self.url}/{kind}"
-    conn = Client.HTTPSConnection(API_HOST)
-    conn.request("GET", url, headers={ "User-Agent": USER_AGENT })
+    url  = self.url + '/' + thing
+    conn = client.HTTPSConnection(API_HOST)
+    conn.request("GET", url, headers = { "User-Agent": USER_AGENT })
 
     res = conn.getresponse()
-    arr = JSON.loads(res.read())
+    arr = json.loads(res.read())
 
-    if kind == "repos":
+    if thing == "repos":
       for obj in arr:
-        n = obj["name"]
-        d = obj["description"]
-        self.Repos["arr"].append(GithubRepo(n, d))
+        self.repos["arr"].append(obj["name"])
 
-    elif kind == "gists":
+    elif thing == "gists":
       for obj in arr:
-        self.Gists["arr"].append(obj["description"])
+        self.gists["arr"].append(obj["description"])
 
-    elif kind == "followers":
+    elif thing == "followers":
       for obj in arr:
-        self.Followers["arr"].append(obj["login"])
+        self.followers["arr"].append(obj["login"])
 
-    elif kind == "following":
+    elif thing == "following":
       for obj in arr:
-        self.Following["arr"].append(obj["login"])
+        self.following["arr"].append(obj["login"])
 
-parser = ArgParse.ArgumentParser(
-  usage="Github.py [OPTIONS] usernames",
-  description="Commandline app demostrating the Github REST API",
-  epilog="https://github.com/Miqueas/Github-REST-API-Example"
+parser = argparse.ArgumentParser(
+  usage       = "Github.py [OPTIONS] usernames",
+  description = "Commandline app demostrating the Github REST API",
+  epilog      = "https://github.com/Miqueas/Github-REST-API-Example"
 )
 
-parser.add_argument("usernames", nargs='+')
-parser.add_argument("-f", "--followers", action="store_true", default=False)
-parser.add_argument("-F", "--following", action="store_true", default=False)
-parser.add_argument("-r", "--repos",     action="store_true", default=False)
-parser.add_argument("-g", "--gists",     action="store_true", default=False)
+parser.add_argument("usernames", nargs = '+')
+parser.add_argument("-f", "--followers", action = "store_true", default = False)
+parser.add_argument("-F", "--following", action = "store_true", default = False)
+parser.add_argument("-r", "--repos",     action = "store_true", default = False)
+parser.add_argument("-g", "--gists",     action = "store_true", default = False)
 
 args = parser.parse_args()
 
 for name in args.usernames:
   user = GithubUser(name)
 
-  print(f"Name: {user.Name}")
-  print(f"Bio: {user.Bio}")
-  print(f"Link: {user.Link}")
+  print(f"Name: {user.name}")
+  print(f"Bio: {user.bio}")
+  print(f"Link: {user.link}")
 
-  print(f"Public repos: {user.Repos['count']}")
+  print(f"Repos: {user.repos['count']}")
   if args.repos:
-    user.Fetch("repos")
+    user.fetch("repos")
 
-    for i in range(len(user.Repos["arr"])):
-      n = user.Repos['arr'][i].Name
-      d = user.Repos['arr'][i].Desc
-      print(f"| {i + 1}. {n}: {d}")
+    for i in range(len(user.repos['arr'])):
+      print(f"| {i + 1}. {user.repos['arr'][i]}")
 
-  print(f"Public gists: {user.Gists['count']}")
+  print(f"Gists: {user.gists['count']}")
   if args.gists:
-    user.Fetch("gists")
+    user.fetch("gists")
 
-    for i in range(len(user.Gists["arr"])):
-      print(f"| {i + 1}. {user.Gists['arr'][i]}")
+    for i in range(len(user.gists["arr"])):
+      print(f"| {i + 1}. {user.gists['arr'][i]}")
 
-  print(f"Followers: {user.Followers['count']}")
+  print(f"Followers: {user.followers['count']}")
   if args.followers:
-    user.Fetch("followers")
+    user.fetch("followers")
 
-    for i in range(len(user.Followers["arr"])):
-      print(f"| @{user.Followers['arr'][i]}")
+    for i in range(len(user.followers["arr"])):
+      print(f"| @{user.followers['arr'][i]}")
 
-  print(f"Following: {user.Following['count']}")
+  print(f"Following: {user.following['count']}")
   if args.following:
-    user.Fetch("following")
+    user.fetch("following")
 
-    for i in range(len(user.Following["arr"])):
-      print(f"| @{user.Following['arr'][i]}")
+    for i in range(len(user.following["arr"])):
+      print(f"| @{user.following['arr'][i]}")
 
   print()
