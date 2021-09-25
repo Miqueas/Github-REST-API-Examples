2f64b797a0f7eb619b68cb46a1176557cae27a4c
Miqueas Martinez <miqueas2020@yahoo.com>
2021-09-25 12:01:40 -0500

    Cambios menores en el código Go

--- Github.go
+++ Github.go
@@ -1,207 +1,196 @@
 package main
 
 import (
-  OS "os"
-  Fmt "fmt"
-  Flag "flag"
-  Str "strings"
-  HTTP "net/http"
-  IOUtil "io/ioutil"
-  JSON "encoding/json"
+	"encoding/json"
+	"io/ioutil"
+	"net/http"
+	"strings"
+	"flag"
+	"fmt"
+	"os"
 )
 
-const APIUsersURL = "https://api.github.com/users/"
+const API_USERS_URL = "https://api.github.com/users/"
 
-var ShowFollowers = Flag.Bool("followers", false, "Show a list of who follow this user")
-var ShowFollowing = Flag.Bool("following", false, "Show a list of who the user follow")
-var ShowRepos     = Flag.Bool("repos", false, "Show a list of public user repos")
-var ShowGists     = Flag.Bool("gists", false, "Show a list of public user gists")
+var show_repos     = flag.Bool("repos",     false, "Show a list of public user repos")
+var show_gists     = flag.Bool("gists",     false, "Show a list of public user gists")
+var show_followers = flag.Bool("followers", false, "Show a list of who follow this user")
+var show_following = flag.Bool("following", false, "Show a list of who the user follow")
 
-type GithubUser struct {
-  // Private
-  dec *JSON.Decoder
-  obj map[string]interface{}
-  url string
-  // Public
-  Name string
-  Bio string
-  Link string
-  ReposCount float64
-  GistsCount float64
-  FollowersCount float64
-  FollowingCount float64
-  ReposArr []map[string]string
-  GistsArr []string
-  FollowersArr []string
-  FollowingArr []string
+type GthUserItem struct {
+	Count float64
+	Arr   []string
 }
 
-// Error checking function
-func Check(e error, msg string, args ...interface{}) {
-  if e != nil {
-    Fmt.Printf(msg, args...)
-    panic(e)
-  }
+type GthUser struct {
+	// Private
+	dec *json.Decoder
+	obj map[string]interface{}
+	url string
+	// Public
+	Name      string
+	Bio       string
+	Link      string
+	Repos     *GthUserItem
+	Gists     *GthUserItem
+	Followers *GthUserItem
+	Following *GthUserItem
+}
+
+func check(e error, msg string, args ...interface{}) {
+	if e != nil {
+		fmt.Printf(msg, args...)
+		panic(e)
+	}
 }
 
 func main() {
-  Flag.Parse()
+	flag.Parse()
 
-  var args = Flag.Args()
-  var argc = len(args)
+	var args = flag.Args()
+	var argc = len(args)
 
-  if argc == 0 {
-    Fmt.Println("No arguments, exiting!")
-    OS.Exit(1)
-  } else {
-    for _, v := range args {
-      var user = NewGithubUser(v)
+	if argc == 0 {
+		fmt.Println("No arguments, exiting!")
+		os.Exit(1)
+	} else {
+		for _, v := range args {
+			var user = NewGthUser(v)
 
-      Fmt.Printf(
-        "Name: %s\nBio: %s\nLink: %s\n",
-        user.Name, user.Bio, user.Link,
-      )
+			fmt.Printf(
+				"Name: %s\nBio: %s\nLink: %s\n",
+				user.Name, user.Bio, user.Link,
+			)
 
-      Fmt.Printf("Public repos: %.0f\n", user.ReposCount)
-      if *ShowRepos {
-        user.Fetch("repos")
+			fmt.Printf("Public repos: %.0f\n", user.Repos.Count)
+			if *show_repos {
+				user.Fetch("repos")
 
-        if len(user.ReposArr) != 0 {
-          for i, v := range user.ReposArr {
-            Fmt.Printf("| %03d. %s: %s\n", i + 1, v["Name"], v["Desc"])
-          }
-        }
-      }
+				if len(user.Repos.Arr) != 0 {
+					for i, v := range user.Repos.Arr {
+						fmt.Printf("| %03d. %s\n", i+1, v)
+					}
+				}
+			}
 
-      Fmt.Printf("Public gists: %.0f\n", user.GistsCount)
-      if *ShowGists {
-        user.Fetch("gists")
+			fmt.Printf("Public gists: %.0f\n", user.Gists.Count)
+			if *show_gists {
+				user.Fetch("gists")
 
-        if len(user.GistsArr) != 0 {
-          for i, v := range user.GistsArr {
-            Fmt.Printf("| %03d. %s\n", i + 1, v)
-          }
-        }
-      }
+				if len(user.Gists.Arr) != 0 {
+					for i, v := range user.Gists.Arr {
+						fmt.Printf("| %03d. %s\n", i+1, v)
+					}
+				}
+			}
 
-      Fmt.Printf("Followers: %.0f\n", user.FollowersCount)
-      if *ShowFollowers {
-        user.Fetch("followers")
+			fmt.Printf("Followers: %.0f\n", user.Followers.Count)
+			if *show_followers {
+				user.Fetch("followers")
 
-        if len(user.FollowersArr) != 0 {
-          for _, v := range user.FollowersArr {
-            Fmt.Printf("| @%s\n", v)
-          }
-        }
-      }
+				if len(user.Followers.Arr) != 0 {
+					for _, v := range user.Followers.Arr {
+						fmt.Printf("| @%s\n", v)
+					}
+				}
+			}
 
-      Fmt.Printf("Following: %.0f\n", user.FollowingCount)
-      if *ShowFollowing {
-        user.Fetch("following")
+			fmt.Printf("Following: %.0f\n", user.Following.Count)
+			if *show_following {
+				user.Fetch("following")
 
-        if len(user.FollowingArr) != 0 {
-          for _, v := range user.FollowingArr {
-            Fmt.Printf("| @%s\n", v)
-          }
-        }
-      }
+				if len(user.Following.Arr) != 0 {
+					for _, v := range user.Following.Arr {
+						fmt.Printf("| @%s\n", v)
+					}
+				}
+			}
 
-      Fmt.Println()
-      user = nil
-    }
-  }
+			fmt.Println()
+			user = nil
+		}
+	}
 }
 
-func NewGithubUser(username string) *GithubUser {
-  var self = &GithubUser{}
-
-  var err error
-  var url = APIUsersURL + username
-  self.url = url
-
-  res, err := HTTP.Get(url)
-  Check(err, "Error fetching url: %s\n", url)
-
-  defer res.Body.Close()
-
-  cont, err := IOUtil.ReadAll(res.Body)
-  Check(err, "Error reading response content.\n")
-
-  var read = Str.NewReader(string(cont))
-
-  self.dec = JSON.NewDecoder(read)
-  Check(self.dec.Decode(&self.obj), "Error decoding the JSON response.\n")
-
-  self.Name          , _ = self.obj["name"].(string)
-  self.Bio           , _ = self.obj["bio"].(string)
-  self.Link          , _ = self.obj["html_url"].(string)
-  self.ReposCount    , _ = self.obj["public_repos"].(float64)
-  self.GistsCount    , _ = self.obj["public_gists"].(float64)
-  self.FollowersCount, _ = self.obj["followers"].(float64)
-  self.FollowingCount, _ = self.obj["following"].(float64)
-
-  return self
+func NewGthUserItem(count float64) *GthUserItem {
+	var self = &GthUserItem{}
+	self.Count = count
+	self.Arr = make([]string, int(count))
+	return self
 }
 
-func (self *GithubUser) Fetch(kind string) {
-  var arr []map[string]interface{}
-  var dec *JSON.Decoder
-  var err error
-  var url = self.url + "/" + kind
+func NewGthUser(username string) *GthUser {
+	var self = &GthUser{}
 
-  res, err := HTTP.Get(url)
-  Check(err, "Error fetching gists.\n")
+	var err error
+	var url = API_USERS_URL + username
+	self.url = url
 
-  defer res.Body.Close()
+	res, err := http.Get(url)
+	check(err, "Error fetching url: %s\n", url)
 
-  cont, err := IOUtil.ReadAll(res.Body)
-  Check(err, "Error reading the gists response.\n")
+	defer res.Body.Close()
 
-  var read = Str.NewReader(string(cont))
-  dec = JSON.NewDecoder(read)
-  Check(dec.Decode(&arr), "Error decoding the gists JSON response.\n")
+	cont, err := ioutil.ReadAll(res.Body)
+	check(err, "Error reading response content.\n")
 
-  switch kind {
-    case "repos":
-      self.ReposArr = make([]map[string]string, len(arr))
+	var read = strings.NewReader(string(cont))
 
-      for i, v := range arr {
-        var name, _ = v["name"].(string)
-        var desc, _ = v["description"].(string)
+	self.dec = json.NewDecoder(read)
+	check(self.dec.Decode(&self.obj), "Error decoding the json response.\n")
 
-        if desc == "" { desc = "(no description)" }
+	self.Name, _ = self.obj["name"].(string)
+	self.Bio,  _ = self.obj["bio"].(string)
+	self.Link, _ = self.obj["html_url"].(string)
 
-        self.ReposArr[i] = map[string]string {
-          "Name": name,
-          "Desc": desc,
-        }
-      }
+	self.Repos     = NewGthUserItem(self.obj["public_repos"].(float64))
+	self.Gists     = NewGthUserItem(self.obj["public_gists"].(float64))
+	self.Followers = NewGthUserItem(self.obj["followers"].(float64))
+	self.Following = NewGthUserItem(self.obj["following"].(float64))
 
-    case "gists":
-      self.GistsArr = make([]string, len(arr))
+	return self
+}
 
-      for i, v := range arr {
-        var desc, _ = v["description"].(string)
-        self.GistsArr[i] = desc
-      }
+func (self *GthUser) Fetch(thing string) {
+	var arr []map[string]interface{}
+	var dec *json.Decoder
+	var err error
+	var url = self.url + "/" + thing
 
-    case "followers":
-      self.FollowersArr = make([]string, len(arr))
+	res, err := http.Get(url)
+	check(err, "Error fetching %s.\n", thing)
 
-      for i, v := range arr {
-        var username, _ = v["login"].(string)
-        self.FollowersArr[i] = username
-      }
+	defer res.Body.Close()
 
-    case "following":
-      self.FollowingArr = make([]string, len(arr))
+	cont, err := ioutil.ReadAll(res.Body)
+	check(err, "Error reading the %s response.\n", thing)
 
-      for i, v := range arr {
-        var username, _ = v["login"].(string)
-        self.FollowingArr[i] = username
-      }
+	var read = strings.NewReader(string(cont))
+	dec = json.NewDecoder(read)
+	check(dec.Decode(&arr), "Error decoding the %s json response.\n", thing)
 
-    default:
-      Fmt.Println("Unsupported endpoint:", kind)
-  }
+	switch thing {
+	case "repos":
+		for i, v := range arr {
+			self.Repos.Arr[i], _ = v["name"].(string)
+		}
+
+	case "gists":
+		for i, v := range arr {
+			self.Gists.Arr[i], _ = v["description"].(string)
+		}
+
+	case "followers":
+		for i, v := range arr {
+			self.Followers.Arr[i], _ = v["login"].(string)
+		}
+
+	case "following":
+		for i, v := range arr {
+			self.Following.Arr[i], _ = v["login"].(string)
+		}
+
+	default:
+		fmt.Println("Unsupported endpoint:", thing)
+	}
 }
