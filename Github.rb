e5a3e320496a9b29f32bd535707a4bab5255e0ad
Miqueas Martinez <miqueas2020@yahoo.com>
2021-09-24 23:14:47 -0500

    Simplificado código Ruby

--- Github.rb
+++ Github.rb
@@ -3,82 +3,7 @@ require 'optparse'
 require 'ostruct'
 require 'json'
 
-APIUsersURL = 'https://api.github.com/users/'
-
-class GithubUser
-  private
-  attr_accessor :Url, :Json
-
-  public
-  attr_accessor :Name, :Bio, :Link
-  attr_accessor :ReposCount, :GistsCount, :FollowersCount, :FollowingCount
-  attr_accessor :ReposArr, :GistsArr, :FollowersArr, :FollowingArr
-
-  def initialize(username)
-    type_err = TypeError.new "Bad argument for 'new'. String expected, got #{username.class}"
-    raise type_err if !username.is_a? String
-
-    @Url = URI APIUsersURL + username
-
-    begin
-      res = Net::HTTP.get_response @Url
-      @Json = JSON.parse res.body
-    rescue => err
-      puts "Something's went wrong! Here's some details:"
-      puts err.message
-    end
-
-    @Name           = @Json['name']         || ''
-    @Bio            = @Json['bio']          || ''
-    @Link           = @Json['html_url']     || ''
-    @ReposCount     = @Json['public_repos'] || 0
-    @GistsCount     = @Json['public_gists'] || 0
-    @FollowersCount = @Json['followers']    || 0
-    @FollowingCount = @Json['following']    || 0
-    @ReposArr       = []
-    @GistsArr       = []
-    @FollowersArr   = []
-    @FollowingArr   = []
-  end
-
-  def Fetch(kind)
-    type_err = TypeError.new "Bad argument for 'Fetch'. String expected, got #{kind.class}"
-    raise type_err if !kind.is_a? String
-
-    begin
-      url = URI @Url.to_s + '/' + kind
-      res = Net::HTTP.get_response url
-      arr = JSON.parse res.body
-    rescue => err
-      puts "Something's went wrong! Here's some details:"
-      puts err.message
-    end
-
-    case kind
-      when 'repos'
-        for val in arr do
-          @ReposArr.push({
-            Name: val['name'],
-            Desc: val['description'] || "(no description)"
-          })
-        end
-      when 'gists'
-        for val in arr do
-          @GistsArr.push val['description']
-        end
-      when 'followers'
-        for val in arr do
-          @FollowersArr.push '@' + val['login']
-        end
-      when 'following'
-        for val in arr do
-          @FollowingArr.push '@' + val['login']
-        end
-      else
-        puts "Unsupported endpoint: #{kind}"
-    end
-  end
-end
+API_USERS_URL = 'https://api.github.com/users/'
 
 opts = { Followers: false, Following: false, Repos: false, Gists: false }
 
@@ -90,6 +15,74 @@ OptionParser.new do |opt|
   opt.on('-g', '--gists', 'Shows user gists') { opts[:Gists] = true }
 end.parse!
 
+class GithubUser
+  private
+  attr_accessor :url, :json
+
+  public
+  attr_accessor :name, :bio, :link
+  attr_accessor :repos, :gists, :followers, :following
+
+  def initialize(username)
+    type_err = TypeError.new "Bad argument for 'new'. String expected, got #{username.class}"
+    raise type_err if !username.is_a? String
+
+    @url = URI API_USERS_URL + username
+
+    begin
+      res = Net::HTTP.get_response @url
+      @json = JSON.parse res.body
+    rescue => err
+      puts "Something went wrong! Here's some details:"
+      puts err.message
+    end
+
+    @name           = @json['name']     || ''
+    @bio            = @json['bio']      || ''
+    @link           = @json['html_url'] || ''
+
+    @repos     = { count: @json['public_repos'] || 0, arr: [] }
+    @gists     = { count: @json['public_gists'] || 0, arr: [] }
+    @followers = { count: @json['followers']    || 0, arr: [] }
+    @following = { count: @json['following']    || 0, arr: [] }
+  end
+
+  def fetch(thing)
+    type_err = TypeError.new "Bad argument for 'Fetch'. String expected, got #{thing.class}"
+    raise type_err if !thing.is_a? String
+
+    begin
+      url = URI @url.to_s + '/' + thing
+      res = Net::HTTP.get_response url
+      arr = JSON.parse res.body
+    rescue => err
+      puts "Something's went wrong! Here's some details:"
+      puts err.message
+    end
+
+    case thing
+      when 'repos'
+        for val in arr do
+          @repos[:arr].push val['name']
+        end
+      when 'gists'
+        for val in arr do
+          @gists[:arr].push val['description']
+        end
+      when 'followers'
+        for val in arr do
+          @followers[:arr].push '@' + val['login']
+        end
+      when 'following'
+        for val in arr do
+          @following[:arr].push '@' + val['login']
+        end
+      else
+        puts "Unsupported endpoint: #{kind}"
+    end
+  end
+end
+
 if ARGV.size == 0
   err = OptionParser::ParseError.new 'see -h or --help for details'
   err.reason = 'No arguments'
@@ -98,43 +91,43 @@ else
   for arg in ARGV
     user = GithubUser.new arg
 
-    puts "Name: #{user.Name}"
-    puts "Bio: #{user.Bio}"
-    puts "Link: #{user.Link}"
+    puts "Name: #{user.name}"
+    puts "Bio: #{user.bio}"
+    puts "Link: #{user.link}"
 
-    puts "Public repos: #{user.ReposCount}"
+    puts "Public repos: #{user.repos[:count]}"
     if opts[:Repos]
-      user.Fetch 'repos'
+      user.fetch 'repos'
 
-      for i in 0 ... user.ReposArr.size
-        puts "| #{i + 1} #{user.ReposArr[i][:Name]}: #{user.ReposArr[i][:Desc]}"
+      for i in 0 ... user.repos[:arr].size
+        puts "| #{i + 1} #{user.repos[:arr][i]}"
       end
     end
 
-    puts "Public gists: #{user.GistsCount}"
+    puts "Public gists: #{user.gists[:count]}"
     if opts[:Gists]
-      user.Fetch 'gists'
+      user.fetch 'gists'
 
-      for i in 0 ... user.GistsArr.size
-        puts "| #{i + 1}. #{user.GistsArr[i]}"
+      for i in 0 ... user.gists[:arr].size
+        puts "| #{i + 1}. #{user.gists[:arr][i]}"
       end
     end
 
-    puts "Followers: #{user.FollowersCount}"
+    puts "Followers: #{user.followers[:count]}"
     if opts[:Followers]
-      user.Fetch 'followers'
+      user.fetch 'followers'
 
-      for v in user.FollowersArr
-        puts "| #{user.FollowersArr.index(v) + 1}. #{v}"
+      for v in user.followers[:arr]
+        puts "| #{user.followers[:arr].index(v) + 1}. #{v}"
       end
     end
 
-    puts "Following: #{user.FollowingCount}"
+    puts "Following: #{user.following[:count]}"
     if opts[:Following]
-      user.Fetch 'following'
+      user.fetch 'following'
 
-      for v in user.FollowingArr
-        puts "| #{user.FollowingArr.index(v) + 1}. #{v}"
+      for v in user.following[:arr]
+        puts "| #{user.following[:arr].index(v) + 1}. #{v}"
       end
     end
 
