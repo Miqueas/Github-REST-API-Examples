cfb0e72a0b483e90746e0c70cc2fa3131575c5a8
Miqueas Martinez <miqueas2020@yahoo.com>
2021-10-21 22:09:33 -0500

    Added constructor and some imports

--- Github.nim
+++ Github.nim
@@ -1,15 +1,45 @@
 # TODO: write the example
+import std/httpclient
+import std/json
+import std/uri
+
+const BASE_URL = parseUri("https://api.github.com/users")
 
 type
-  GthUserItem = ref object
-    count: int
-    arr: seq[string]
+  GthUserItem* = ref object
+    count*: int
+    arr*: seq[string]
 
-  GthUser = ref object
-    name: string
-    bio: string
-    link: string
-    repos: GthUserItem
-    gists: GthUserItem
-    followers: GthUserItem
-    following: GthUserItem
+  GthUser* = ref object
+    # Private
+    obj: JsonNode
+    url: Uri
+    # Public
+    name*: string
+    bio*: string
+    link*: string
+    repos*: GthUserItem
+    gists*: GthUserItem
+    followers*: GthUserItem
+    following*: GthUserItem
+
+proc newGthUser*(username: string): GthUser =
+  var self = GthUser()
+  self.url = parseUri($(BASE_URL / username))
+
+  var res  = newHttpClient().getContent($self.url)
+  self.obj = parseJson(res)
+
+  self.name = self.obj["name"].getStr()
+  self.bio  = self.obj["bio"].getStr()
+  self.link = self.obj["html_url"].getStr()
+
+  self.repos = GthUserItem(count: self.obj["public_repos"].getInt())
+  self.gists = GthUserItem(count: self.obj["public_gists"].getInt())
+  self.followers = GthUserItem(count: self.obj["followers"].getInt())
+  self.following = GthUserItem(count: self.obj["following"].getInt())
+
+  return self
+
+var user = newGthUser("Miqueas")
+echo user[]
