958e451997f668f757ab33a929df9efa5af56d95
Miqueas Martinez <miqueas2020@yahoo.com>
2021-10-21 21:10:25 -0500

    Added some initial code for the Nim example

--- Github.nim
+++ Github.nim
@@ -1,1 +1,15 @@
 # TODO: write the example
+
+type
+  GthUserItem = ref object
+    count: int
+    arr: seq[string]
+
+  GthUser = ref object
+    name: string
+    bio: string
+    link: string
+    repos: GthUserItem
+    gists: GthUserItem
+    followers: GthUserItem
+    following: GthUserItem
